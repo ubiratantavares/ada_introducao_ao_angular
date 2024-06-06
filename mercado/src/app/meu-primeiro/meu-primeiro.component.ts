fb09e40c2b21e5487dc351d9cf7f1d749ff86025
Ubiratan Tavares <ust1973@gmail.com>
2024-06-06 09:28:40 -0300

    criando componente e modulo

---
+++ mercado/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-primeiro.component.html',
+  styleUrl: './meu-primeiro.component.css'
+})
+export class MeuPrimeiroComponent {
+
+}
